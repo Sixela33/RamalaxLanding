01380b1ca320587b5f243ae8373f56699b99c28c
sixela33 <alexis.janko@gmail.com>
2024-10-29 16:36:23 -0300

    finished popup

--- src/components/JoinWaitlist.tsx
+++ src/components/JoinWaitlist.tsx
@@ -33,25 +33,31 @@ export default function JoinWaitlist({ isOpen, setIsOpen }: JoinWaitlistProps) {
     }
   };
 
+  const handleClose = () => {
+    setMessage('')
+    setIsOpen(false)
+  }
+
   return (
     <>
       {isOpen && (
         <div 
-          onClick={() => setIsOpen(false)}
+          onClick={handleClose}
           className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50 transition-opacity duration-300">
           <div 
             onClick={e => e.stopPropagation()}
-            className="bg-red bg-opacity-100 text-white p-8 rounded-lg w-11/12 max-w-md shadow-lg ">
+            className="bg-gray-900 text-white p-8 rounded-lg w-11/12 max-w-md shadow-xl ">
             <div className="flex justify-between items-center mb-6">
               <h2 className="text-2xl font-semibold">Join the Waitlist</h2>
               <button
-                onClick={() => setIsOpen(false)}
-                className="text-gray-300 hover:text-red-500 transition-transform transform hover:scale-125 duration-200"
+                onClick={handleClose}
+                className="text-gray-400 hover:text-gray-200 transition-all duration-200 hover:rotate-90"
               >
                 X
               </button>
             </div>
-            <h4>
+            
+            <h4 className="text-gray-300 mb-6">
               Be part of the future
             </h4>
 
