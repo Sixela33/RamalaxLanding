96b9f7d604cf4d94816fd88b4760ed7b1f3e7100
sixela33 <alexis.janko@gmail.com>
2024-10-29 17:11:09 -0300

    adding feedback to user

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -9,7 +9,9 @@ export function ContactForm() {
     message: '',
   });
 
-  const handleChange = (e:any) => {
+  const [feedbackMessage, setFeedbackMessage] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -20,7 +22,7 @@ export function ContactForm() {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
-    const { name, email, company, phone, message } = formData
+    const { name, email, company, phone, message } = formData;
 
     const dataToSend = {
       name,
@@ -39,17 +41,26 @@ export function ContactForm() {
         },
         body: JSON.stringify(dataToSend),
       });
-  
+
       const data = await response.json();
-  
+
       if (data.success) {
+        setFeedbackMessage("Your message has been sent successfully!");
+        // Clear the form if desired
+        setFormData({
+          name: '',
+          email: '',
+          company: '',
+          phone: '',
+          message: '',
+        });
       } else {
         console.error("Error submitting form:", data);
-        ("There was an error submitting the form. Please try again.");
+        setFeedbackMessage("There was an error submitting the form. Please try again.");
       }
     } catch (error) {
       console.error("Error submitting form:", error);
-      ("An unexpected error occurred. Please try again.");
+      setFeedbackMessage("An unexpected error occurred. Please try again.");
     }
 
     console.log('Form submitted:', formData);
@@ -57,67 +68,70 @@ export function ContactForm() {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
-        <div className='bg-gray-900 rounded-2xl p-12 text-center'>
-            <h2 className="text-3xl font-bold text-center mb-12">Contact Us</h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
-                        required
-                        />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
-                        required
-                        />
-                    <input
-                        type="text"
-                        name="company"
-                        placeholder="Company"
-                        value={formData.company}
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
-                        />
-                    <input
-                        type="tel"
-                        name="phone"
-                        placeholder="Phone"
-                        value={formData.phone}
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
-                        />
-                    <textarea
-                        name="message"
-                        placeholder="Message"
-                        value={formData.message}
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors md:col-span-2"
-                        rows={4}
-                        required
-                        />
-                    </div>
-                    <div className='flex flex-row'>
-                        <button
-                            type="submit"
-                            className="w-1/2 px-4 py-2 rounded-lg text-white transition-colors bg-green-500"
-                            >
-                            Leave a message
-                        </button>
-                        <a href='https://calendly.com/alexis-janko/30min' className='w-1/2 px-4 py-2 rounded-lg text-white transition-colors bg-green-500' target='#'> 
-                                Arrange a meeting
-                        </a>
-                    </div>
-            </form>
-        </div>
+      <div className='bg-gray-900 rounded-2xl p-12 text-center'>
+        <h2 className="text-3xl font-bold text-center mb-12">Contact Us</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
+              required
+            />
+            <input
+              type="text"
+              name="company"
+              placeholder="Company"
+              value={formData.company}
+              onChange={handleChange}
+              className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
+            />
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Phone"
+              value={formData.phone}
+              onChange={handleChange}
+              className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors"
+            />
+            <textarea
+              name="message"
+              placeholder="Message"
+              value={formData.message}
+              onChange={handleChange}
+              className="w-full px-4 py-2 bg-gray-700 text-gray-300 rounded-lg outline-none focus:border focus:border-green-400 transition-colors md:col-span-2"
+              rows={4}
+              required
+            />
+          </div>
+          <div className='flex flex-row'>
+            <button
+              type="submit"
+              className="w-1/2 px-4 py-2 rounded-lg text-white transition-colors bg-green-500"
+            >
+              Leave a message
+            </button>
+            <a href='https://calendly.com/alexis-janko/30min' className='w-1/2 px-4 py-2 rounded-lg text-white transition-colors bg-green-500' target='#'> 
+              Arrange a meeting
+            </a>
+          </div>
+        </form>
+        {feedbackMessage && (
+          <p className="mt-4 text-green-500">{feedbackMessage}</p>
+        )}
+      </div>
     </div>
   );
 }
